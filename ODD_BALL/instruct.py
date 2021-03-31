ff7e7c35294a74cec0a104ff1b74c03ef68fa85a
paul30402 <paul.z.cheng@gmail.com>
2021-03-31 20:25:47 +0800

    edited instruction

--- ODD_BALL/instruct.py
+++ ODD_BALL/instruct.py
@@ -1,24 +1,24 @@
 init_inst = """
-In this experiment we will present blocks of visual stimuli. They will presented one stimulus at a time.  Your task is to press the key corresponding to the matching stimulus as quickly and accurately as possible after each stimulus is presented.  The mappings between stimuli and specific keyboard responses will change for each block.  Either an X or an O.
+In this experiment we will present blocks of visual stimuli. Stimli will presented one at a time in the center of the screen.  Your task is to press the corresponding key to the matching stimulus as quickly and accurately as possible after each presentation.  The mappings between stimulus and specific key responses will change between each block.  Either an X or an O.
 
-We will now review each stimulus prior to beginning the blocks.
+We will now begin by viewing each of stimuli. Welcome to ask experimentor any question if not.
 
 Press any key to continue....
 """
 
-final_inst = """We will now begin the actual experiment.  Before each block we will display a screen specifying what the mapping from the stimuli to the specific keys will be for that block.  Please take a moment before beginning the block to learn the new mapping. Remember, you need to wait after the stimulus is off the screen, and press the corresponding button.
+final_inst = """We will now begin the actual experiment.  Before each block begin, mapping of key and stimulus pair will be display on screen.  Please take a moment before beginning of each block to learn the new mapping. Remember, you need to wait after the stimulus is off the screen, and then press the corresponding key.
 
-If you still not clear about any part of instruction, please ask adminstor before began.
+If you are still not clear about any part of the instruction, please ask experimentor now.
 
 Press any key to continue...
 """
 
-finished_inst = """ Thank you for participate in our experiment.
+finished_inst = """ Thank you for your participation in our experiment.
 
 If you have any question regarding this experiment,
 please feel free to ask or email us at
 
 paul.z.cheng@gmail.com
 
-Press any Key to end the experiment.
+Press any key to end the experiment.
 """
