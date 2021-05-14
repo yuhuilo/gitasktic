0f53d020c5b32a1f49c3af0c71815ea1952ccc9e
Nick F. De la Cruz <ndelacruz@protonmail.com>
2021-05-14 20:47:49 +0800

    fixed the issues with smaller numbers inputed at the end of the order. Also added name == main.

--- miss_num_ultimate.py
+++ miss_num_ultimate.py
@@ -33,18 +33,18 @@ def find_miss_num_func():
     a = input("Take in numbers(press space key to space number, ex: 0 1 2 3 4)\nEnter here: ")
     # create a list of the input numbers
     a_split = a.split(sep=' ')
-    a_list = list(map(int, a_split))
-    
-    
+    a_list = sorted(map(int, a_split))
+
+
     #find miss number with miss_num_func
     miss = miss_num_func(a_list)
-    
+
     miss_to_str = ' '.join(map(str, miss))
     if len(miss) != 1:
         print("There are multilple missing numbers: %s" % miss_to_str )
-    
+
     elif len(miss) == 1:
         print("There is one missing number: %s" % miss_to_str)
-        
-find_miss_num_func()
 
+if __name__ == '__main__':
+    find_miss_num_func()
