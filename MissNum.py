d5a576e172e5030a5ecfe9b876668accde1791ee
yuhui <yuhuilo4235@gmail.com>
2021-06-04 11:03:50 +0800

    print other list

--- MissNum.py
+++ MissNum.py
@@ -1,8 +1,12 @@
 def find_missing(lst):
-    #return[ x for x in range(lst[0], max(lst)+1)
-    return [x for x in range(lst[0], lst[-1]+1)
-                               if x not in lst]
-  
+    return[x for x in range(min(lst), max(lst)+1)
+           # return [x for x in range(lst[0], lst[-1]+1)
+           if x not in lst]
+
+
 # Driver code
 lst = [1, 2, 6, 4, 12, 9, 10]
 print(find_missing(lst))
+
+lst2 = [1, 2, 10]
+print(find_missing(lst2))
